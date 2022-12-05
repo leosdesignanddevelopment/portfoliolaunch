13dca581c0d856145e3a9c078a5c1ee0dc1130f7
leosdesignanddevelopment <118224536+leosdesignanddevelopment@users.noreply.github.com>
2022-12-05 10:01:23 -0600

    Add files via upload

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -29,33 +29,34 @@ function Skills() {
     <Container fluid>
     <Row className="skillsRow justify-content-start align-items-center text-center">
     
-      <Col className='text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ' >
+      <Col className='text-center  ' >
         <img src={htmlSvg} className="img-fluid" alt="HTML5 icon" />
         <h3>HTML5</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 " >
+      <Col className="text-center  " >
         <img src={cssSvg} className="img-fluid" alt="CSS Icon" />
         <h3>CSS3</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 " >
+      <Col className="text-center  " >
         <img src={javascriptSvg} className="img-fluid" alt="JavaScript Icon" />
         <h3>JavaScript</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4 col-lg-2 " >
+    </Row>
+
+
+    <Row className="skillsRow justify-content-start align-items-center text-center">
+    <Col className="text-center" >
         <img src={jquerySvg} className="img-fluid" alt="JQuery Icon" />
         <h3>JQuery</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2" >
+      <Col className="text-center " >
         <img src={reactSvg} className="img-fluid" alt="React Icon" />
         <h3>React</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={bootstrapSvg} className="img-fluid" alt="BootStrap5 icon" />
         <h3>BootStrap 5</h3>
       </Col>
-
-
-
 
     </Row>
 
@@ -63,27 +64,31 @@ function Skills() {
     <Row className="skillsRow justify-content-start align-items-center text-center">
      
      
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={sassSvg} className="img-fluid" alt="Sass Icon" />
         <h3>Sass</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={wordpressSvg} className="img-fluid" alt="WordPress Icon" />
         <h3>WordPress</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={phpSvg} className="img-fluid" alt="PHP Icon" />
         <h3>PHP</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+    
+    </Row>
+
+    <Row className="skillsRow justify-content-start align-items-center text-center">
+    <Col className="text-center  ">
         <img src={sqlSvg} className="img-fluid" alt="SQL Icon" />
         <h3>SQL</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
           <img src={premiereSvg} className="img-fluid" alt="premiere5 icon" />
           <h3>Premiere Pro</h3>
         </Col>
-        <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+        <Col className="text-center  ">
           <img src={aftereffectsSvg} className="img-fluid" alt="aftereffects Icon" />
           <h3>After Effects</h3>
         </Col>
@@ -98,15 +103,15 @@ function Skills() {
       
        
         
-        <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+        <Col className="text-center  ">
         <img src={indesignSvg} className="img-fluid" alt="indesign Icon" />
         <h3>Indesign</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={photoshopSvg} className="img-fluid" alt="photoshop Icon" />
         <h3>PhotoShop</h3>
       </Col>
-      <Col className="text-center col-xs-4 col-sm-4 col-md-4  col-lg-2 ">
+      <Col className="text-center  ">
         <img src={figmaSvg} className="img-fluid" alt="figma Icon" />
         <h3>Figma</h3>
       </Col>
