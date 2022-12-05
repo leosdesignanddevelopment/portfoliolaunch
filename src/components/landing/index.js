79c03bce1096ed90ca4018a669bfb9515b197505
leosdesignanddevelopment <118224536+leosdesignanddevelopment@users.noreply.github.com>
2022-12-05 10:18:34 -0600

    Update index.js

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -18,8 +18,8 @@ class Landing extends React.Component {
     return (
       <div className='main-content d-flex align-items-center '>
       <Container className={this.state.animate ? "fadeIn item" : "item"}>
-         <p className='ms-5 item'>A Portfolio</p>
-         <h2 className='ms-5 item'>By: Omar Morales  </h2>
+         <p className='ms-5 item'>Omar Morales</p>
+         <h2 className='ms-5 item'>(Scroll to explore) </h2>
          
       </Container>
       </div>
@@ -39,68 +39,7 @@ class Landing extends React.Component {
 
 
 
-
-
-  // componentDidMount(){
-  //   // Check if visited
-  //   if (window.sessionStorage.getItem("key") === null) {
-  //     window.sessionStorage.setItem("key", 1)
-
-  //     this.setState({
-  //       animate: false
-      
-  //     })
-
-  //     console.log("First Token Loaded")
-  //   } else {
-  //     this.setState({
-  //       animate: true
-  //     })
-  //     console.log("Token Exist")
-  //   }
-
-
-  // }
-
-
 }
 
-    // sessionStorage.setItem('key', 'value');
-    // let data = sessionStorage.getItem('key');
-
-    // // Removal
-    // sessionStorage.removeItem('key')
-    // sessionStorage.clear();
-
-
-    // console.log("loaded")
-    // // Check if visited
-    // if (window.sessionStorage.getItem("firstLoadDone") == 1) {
-    //   this.setState({
-    //     animate: true
-    //   })
-    //   window.sessionStorage.setItem("firstLoadDone", null)
-    // } else {
-    //   this.setState({
-    //     animate:false
-    //   })
-    // }
-// function Landing() {
-
-
-
-//   return (
-//   <div className='main-content d-flex align-items-center fadeIn '>
-//   <Container>
-//        <p className='ms-5 '>A Portfolio</p>
-//       <h2 className='ms-5'>By: Omar Morales  </h2>
-
-     
-
-   
-//   </Container>
-// </div>
-//   )
-// }
 
 export default Landing
